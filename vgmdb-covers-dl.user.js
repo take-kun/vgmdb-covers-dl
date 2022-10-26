5b697a6332c03f2e2c259d0738db8488870187ca
take-kun <109226194+take-kun@users.noreply.github.com>
2022-10-27 00:42:55 +0200

    Throttle requests and prevent race conditions

--- vgmdb-covers-dl.user.js
+++ vgmdb-covers-dl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name VGMdb Covers Downloader
 // @namespace https://github.com/take-kun
-// @version 0.1.0
+// @version 0.2.0
 // @description Tool for automatically downloading all covers available for the release on VGMdb.net
 // @author Take-kun
 // @license GPL-3.0
@@ -34,31 +34,39 @@
     dl.append(getDLButton().content);
     dl.onclick = (e) => {
         e.preventDefault();
-        [...links].forEach((l, i) => {
-            fetch(l.href)
-                .then(res => res.text())
-                .then(html => {
-                    const parser = new DOMParser();
-                    const htmlDOM = parser.parseFromString(html, "text/html");
-                    return htmlDOM.querySelector('#scrollpic').src;
-                })
-                .then(src => GM_xmlhttpRequest({
-                    url: src,
-                    responseType: 'blob',
-                    onload: ({response}) => {
-                        const url = URL.createObjectURL(response);
-                        const link = document.createElement('a');
-                        link.href = url;
-                        link.download = l.text;
-                        document.body.appendChild(link);
-                        link.click();
-                        setTimeout(() => {
-                            document.body.removeChild(link);
-                            URL.revokeObjectURL(url);
-                        }, 0);
-                    },
-                }));
-        })
+        (function loop() {
+            setTimeout(() => {
+                const next = links.next();
+                if (next.done) {
+                    return;
+                }
+                const l = next.value;
+                fetch(l.href)
+                    .then(res => res.text())
+                    .then(html => {
+                        const parser = new DOMParser();
+                        const htmlDOM = parser.parseFromString(html, "text/html");
+                        return htmlDOM.querySelector('#scrollpic').src;
+                    })
+                    .then(src => GM_xmlhttpRequest({
+                        url: src,
+                        responseType: 'blob',
+                        onload: ({ response }) => {
+                            const url = URL.createObjectURL(response);
+                            const link = document.createElement('a');
+                            link.href = url;
+                            link.download = l.text;
+                            document.body.appendChild(link);
+                            link.click();
+                            setTimeout(() => {
+                                document.body.removeChild(link);
+                                URL.revokeObjectURL(url);
+                            }, 0);
+                        },
+                    }));
+                loop();
+            }, 500);
+        })();
     };
 
     const coversListEl = document.querySelector('#cover_list');
